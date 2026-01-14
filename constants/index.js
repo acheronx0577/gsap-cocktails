90ba605bdc8909a5bddf5b415cbd0c78718277fe
AcheronX. <richardhuynh53@gmail.com>
2026-01-14 14:50:42 -0800

    feat: define navigation link constants

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+const navLinks = [
+ {
+	id: "cocktails",
+	title: "Cocktails",
+ },
+ {
+	id: "about",
+	title: "About Us",
+ },
+ {
+	id: "work",
+	title: "The Art",
+ },
+ {
+	id: "contact",
+	title: "Contact",
+ },
+];
+
+export {
+ navLinks,
+};
