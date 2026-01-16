2dfe55be666a9f8437a2e6c454b912cd8ba94393
AcheronX. <richardhuynh53@gmail.com>
2026-01-15 21:37:55 -0800

    feat: define art section feature lists

--- constants/index.js
+++ constants/index.js
@@ -86,9 +86,25 @@ const profileLists = [
  },
 ];
 
+const featureLists = [
+ "Perfectly balanced blends",
+ "Garnished to perfection",
+ "Ice-cold every time",
+ "Expertly shaken & stirred",
+];
+
+const goodLists = [
+ "Handpicked ingredients",
+ "Signature techniques",
+ "Bartending artistry in action",
+ "Freshly muddled flavors",
+];
+
 export {
  navLinks,
  cocktailLists,
  mockTailLists,
  profileLists,
+ featureLists,
+ goodLists,
 };
